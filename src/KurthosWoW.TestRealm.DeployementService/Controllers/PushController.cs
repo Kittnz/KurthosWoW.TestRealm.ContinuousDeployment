e01f79ee7337411c6103dc1c434eb2f8457ffb7f
HelloKitty <andrew.blakely@ymail.com>
2018-09-07 15:45:34 -0500

    Initial Project Setup

---
+++ src/KurthosWoW.TestRealm.DeployementService/Controllers/PushController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KurthosWoW
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PushController : ControllerBase
+	{
+		[HttpGet]
+		public string Get()
+		{
+			return "Online";
+		}
+
+		[HttpPost]
+		public async Task OnRecievedGithubPushEvent()
+		{
+			return;
+		}
+	}
+}
