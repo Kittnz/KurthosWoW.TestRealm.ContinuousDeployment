c3f926bc415b52a7cbd1b72f57ce2ccc411bae3a
HelloKitty <andrew.blakely@ymail.com>
2018-09-07 16:46:32 -0500

    Try to fix artifacts for Appveyor

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
 dotnet publish src/KurthosWoW.TestRealm.DeployementService/KurthosWoW.TestRealm.DeployementService.csproj -c Release -r win10-x64
+mkdir artifacts
+xcopy /s src\KurthosWoW.TestRealm.DeployementService\bin\Release\netcoreapp2.1\win10-x64\publish artifacts
