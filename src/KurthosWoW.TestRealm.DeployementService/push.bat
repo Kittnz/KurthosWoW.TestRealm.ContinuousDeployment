c1439dfe5727d173235bd09d2d6290ea9505226a
HelloKitty <andrew.blakely@ymail.com>
2018-09-09 10:59:49 -0500

    Fixed push batch script

--- src/KurthosWoW.TestRealm.DeployementService/push.bat
+++ src/KurthosWoW.TestRealm.DeployementService/push.bat
@@ -1,6 +1,11 @@
 cd C:\Users\Administrator\Documents\GitHub\KurthosWoW.Trinitycore
 git pull
-taskill /f /im worldserver.exe
-msbuild C:\Users\Administrator\Documents\GitHub\KurthosWoW.Trinitycore\build\src\server\worldserver\worldserver.vcxproj /t:build /p:configuration=release
+
+taskkill /f /im worldserver.exe
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+cd C:\Users\Administrator\Documents\GitHub\KurthosWoW.Trinitycore\build\src\server\worldserver\
+msbuild worldserver.vcxproj /t:build /p:configuration=release
+
 cd C:\Users\Administrator\Documents\GitHub\KurthosWoW.Trinitycore\build\bin\Release
 start worldserver.exe
